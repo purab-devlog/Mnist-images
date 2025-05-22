7141676904af9f75b119f84b569d79bac8d59466
Purab <purabmanoj03@gmail.com>
2025-05-22 21:44:21 +0530

    Training.py
    In this section, we load the MNIST dataset and train a simple Convolutional Neural Network (CNN) for digit classification.
    
    After training, we apply ternary quantization to the weights of the model. Ternarization involves mapping the weights to one of three values: [1 0 -1], using a global threshold derived from the maximum absolute weight value. This reduces the model’s precision and size, making it suitable for deployment on low-resource embedded devices.
    
    Finally, we convert this ternarized Keras model into a TensorFlow Lite model, which can be used for efficient inference on edge devices like microcontrollers.

--- Training.py
+++ Training.py
@@ -5,6 +5,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.utils import to_categorical
 
+
 # Step 1: Load the MNIST dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train = x_train.astype("float32") / 255.0
