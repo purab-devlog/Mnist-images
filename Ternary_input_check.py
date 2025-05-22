57176ac3e848bdc42d065cf2d91f74c118e117e0
Purab <purabmanoj03@gmail.com>
2025-05-22 21:43:28 +0530

    Ternary_input_check.py
    This section focuses on input ternarization. Instead of feeding raw grayscale pixel values into the model, we convert each image into ternary format based on its statistical properties.
    
    For each image, we calculate the mean and standard deviation and assign pixel values to:
    
    +1 if above (mean + 0.25 * std),
    -1 if below (mean - 0.25 * std),
    0 otherwise.
    We then visualize both the original and ternarized images side-by-side. This helps in understanding how input ternarization affects the visual representation and preserves relevant features.

--- Ternary_input_check.py
+++ Ternary_input_check.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
 
+
 (_, _), (x_test, y_test) = mnist.load_data()
 x_test = x_test.astype('float32') / 255.0
 x_test = np.expand_dims(x_test, axis=-1)
